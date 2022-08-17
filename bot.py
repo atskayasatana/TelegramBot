69866951f9a7b125e1f0253e512e568d4f4295ac
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-17 21:27:29 +0300

    добавлен функционал боту(фото есть/случайное фото)

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import random
 import telegram
@@ -6,30 +7,31 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     load_dotenv()
-    token=os.getenv('TELEGRAM_BOT_TOKEN')
-    delay=os.getenv('DELAY_HOURS')
-
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    delay = os.getenv('DELAY_HOURS')
+    directories = [Path.cwd().joinpath("images"),
+                   Path.cwd().joinpath("EPIC"),
+                   Path.cwd().joinpath("APOD")]   
     bot = telegram.Bot(token=token)
-
-    bot.send_message(text="Hello, I'm a space addicted bot", chat_id='@taikong_2022')
-
-
-    directories=[Path.cwd().joinpath("images"),Path.cwd().joinpath("EPIC"),Path.cwd().joinpath("APOD")]
-    dir_num=random.randint(0,2)
-    images=os.listdir(directories[dir_num])
-    random_image=directories[dir_num].joinpath(random.choice(images))
-    bot.send_photo(chat_id='@taikong_2022', photo=open(random_image, 'rb'))
-
-    while True:
-        time.sleep(delay*3600)
-        dir_num=random.randint(0,2)
-        images=os.listdir(directories[dir_num])
-        random_image=directories[dir_num].joinpath(random.choice(images))
+    bot.send_message(text="Hello, I'm a space addicted bot",
+                     chat_id='@taikong_2022')
+    parser = argparse.ArgumentParser()    
+    parser.add_argument('file_to_send',
+                        nargs='?',
+                        default=None)   
+    image_file = parser.parse_args().file_to_send
+    if image_file:
+        bot.send_photo(chat_id='@taikong_2022', photo=open(image_file, 'rb'))
+    else:
+        dir_num = random.randint(0, 2)
+        images = os.listdir(directories[dir_num])
+        random_image = directories[dir_num].joinpath(random.choice(images))
         bot.send_photo(chat_id='@taikong_2022', photo=open(random_image, 'rb'))
-    
-    
-
-
+    while True:
+        time.sleep(int(delay)*3600)
+        dir_num = random.randint(0, 2)
+        images = os.listdir(directories[dir_num])
+        random_image = directories[dir_num].joinpath(random.choice(images))
+        bot.send_photo(chat_id='@taikong_2022', photo=open(random_image, 'rb'))
