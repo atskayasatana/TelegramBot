cb1ac9429e45e2294c72689ea2379454544a7c41
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-17 21:36:30 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,36 +5,26 @@ from functions import fetch_spacex_launch_images
 from functions import get_launch_id
 from pathlib import Path
 
-URL='https://api.spacexdata.com/v5/launches'
+URL = 'https://api.spacexdata.com/v5/launches'
+FOLDER_TO_DOWNLOAD = "Images"
 
-FOLDER_TO_DOWNLOAD="Images"
 
-
-if __name__=='__main__':
-    
+if __name__ == '__main__':    
     parser = argparse.ArgumentParser()
-    
     parser.add_argument('id',
                         help="id of spaceX launch to download images",
                         nargs='?',
                         default=None)
 
     spacex_launch_id = parser.parse_args().id
-    
     if spacex_launch_id:
-        links=get_foto_of_launch_by_id(URL, spacex_launch_id)
+        links = get_foto_of_launch_by_id(URL, spacex_launch_id)
     else:
-        links=get_latest_launch_foto(URL)
+        links = get_latest_launch_foto(URL)
         if not links:
             latest_launch_with_foto_id = get_launch_id(URL)
-            links=get_foto_of_launch_by_id(URL, latest_launch_with_foto_id)
-            
-    path=Path.cwd()
-    download_path=path.joinpath(FOLDER_TO_DOWNLOAD)
+            links = get_foto_of_launch_by_id(URL, latest_launch_with_foto_id)      
+    path = Path.cwd()
+    download_path = path.joinpath(FOLDER_TO_DOWNLOAD)
     Path.mkdir(download_path, exist_ok=True)
     fetch_spacex_launch_images(links, download_path)
-    
-
-    
-        
-    
