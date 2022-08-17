d9be8cf01c3fbd17fda7e943fa21c701452e4b24
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-17 21:30:12 +0300

    Update fetch_epic_images.py

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -5,22 +5,19 @@ import sys
 from dotenv import load_dotenv
 from functions import get_EPIC
 
-
-FOLDER_TO_DOWNLOAD="EPIC"
+FOLDER_TO_DOWNLOAD = "EPIC"
 
 if __name__ == '__main__':
-     load_dotenv()
-     api_key=os.getenv('api_key')
+    load_dotenv()
+    api_key = os.getenv('api_key')
      
-     parser = argparse. ArgumentParser()
-     parser.add_argument('epic_count',
-                         nargs='?',
-                         default=10,
-                         type=int)
+    parser = argparse. ArgumentParser()
+    parser.add_argument('epic_count',
+                        nargs='?',
+                        default=10,
+                        type=int)
 
-     count=int(parser.parse_args().epic_count)
-
-     params={'api_key':api_key
-             }
-     get_EPIC(params,FOLDER_TO_DOWNLOAD, count)
+    count = int(parser.parse_args().epic_count)
+    params = {'api_key': api_key}
+    get_EPIC(params, FOLDER_TO_DOWNLOAD, count)
 
