33eeb5c56b085d12c84c12a7e80c6377b72a954f
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-20 23:31:50 +0300

    Update fetch_picture_of_a_day.py

--- fetch_picture_of_a_day.py
+++ fetch_picture_of_a_day.py
@@ -1,25 +1,27 @@
 import argparse
 import os
 
-from functions import get_APOD
+from APOD_functions import get_APOD
 from dotenv import load_dotenv
 from pathlib import Path
 
-URL = 'https://api.nasa.gov/planetary/apod'
-FOLDER_TO_DOWNLOAD = "APOD"
+URL='https://api.nasa.gov/planetary/apod'
+
+FOLDER_TO_DOWNLOAD="APOD"
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    api_key = os.getenv('api_key')
-    parser = argparse. ArgumentParser()
-    parser.add_argument('image_count',
-                        help="number of images to download",
-                        nargs='?',
-                        default=5)
+     load_dotenv()
+     API_KEY=os.environ['API_KEY']
+     parser = argparse. ArgumentParser()
+     parser.add_argument('image_count',
+                         help="number of images to download",
+                         nargs='?',
+                         default=5)
 
-    count = min(int(parser.parse_args().image_count), 100)
-    params = {'api_key': api_key,
-              'count': count
-              }
-    get_APOD(URL, params, FOLDER_TO_DOWNLOAD)
+     count= min(int(parser.parse_args().image_count), 100)
+     
+     params={'api_key':API_KEY,
+             'count':count
+             }
+     get_APOD(URL,params, FOLDER_TO_DOWNLOAD)
