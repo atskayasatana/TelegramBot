9c535d63d21bb8a5d440692a5864789ef0183b46
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-23 23:32:48 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,16 +1,47 @@
 import argparse
+import requests
 
-from space_x_functions import get_foto_of_launch_by_id
-from space_x_functions import get_latest_launch_foto
-from space_x_functions import fetch_spacex_launch_images
-from space_x_functions import get_launch_id
+from datetime import date, datetime, timedelta
+from functions import download_img
+from functions import parse_date
 from pathlib import Path
+from urllib.parse import unquote, urlencode, urlparse
 
 URL = 'https://api.spacexdata.com/v5/launches'
 
 FOLDER_TO_DOWNLOAD = "Images"
 
 
+def get_foto_of_launch_by_id(url, launch_id='latest'):
+    launch_url=f'{url}/{launch_id}'
+    response = requests.get(launch_url)
+    response.raise_for_status() 
+    return response.json()['links']['flickr']['original']
+
+
+def get_launch_id(url):
+    ids=[]
+    end_year, end_month, end_day = parse_date(date.today())
+    start_year, start_month, start_day = '2022', '01', '01'
+    query = {'start': f'{start_year}-{start_month}-{start_day}',
+             'end': f'{end_year}-{end_month}-{end_day}'
+            }
+    filter_value = urlencode(query)
+    response = requests.get(url, params=filter_value)
+    response.raise_for_status()
+    for launch in response.json():
+        if launch['links']['flickr']['original']:
+            ids.append(launch['id'])   
+    return ids[-1]
+
+
+def fetch_spacex_launch_images(links, path):
+    for num, link in enumerate(links):
+        filename = f'spacex_{num}.jpg'
+        download_path = path.joinpath(filename)
+        download_img(link, download_path)
+
+        
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
@@ -23,7 +54,7 @@ if __name__ == '__main__':
     if spacex_launch_id:
         links = get_foto_of_launch_by_id(URL, spacex_launch_id)
     else:
-        links = get_latest_launch_foto(URL)
+        links = get_foto_of_launch_by_id(URL)
         if not links:
             latest_launch_with_foto_id = get_launch_id(URL)
             links = get_foto_of_launch_by_id(URL, latest_launch_with_foto_id)         
