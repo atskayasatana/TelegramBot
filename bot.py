3ac3c1cabfe2d89016d8f55d38bafe316a2b0f70
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-27 21:59:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from retry import retry
 
 
-@retry(telegram.error.NetworkError, tries=100, delay=2) 
+@retry(telegram.error.NetworkError, tries=100, delay=2)
 def send_message(text, chat_id):
     try:
         bot.send_message(text=text, chat_id=chat_id)
@@ -21,16 +21,14 @@ def send_message(text, chat_id):
         print('Попытка соединения...')
         raise
 
-    
-@retry(telegram.error.NetworkError, tries=100, delay=5) 
+@retry(telegram.error.NetworkError, tries=100, delay=5)
 def send_random_image(img_data):
     try:
         bot.send_photo(chat_id=chat_id, photo=img_data)
     except telegram.error.NetworkError:
         print('Ошибка подключения!')
         print('Попытка соединения...')
-        raise    
-
+        raise
 
 TEXT = 'Hello, I\'m the coolest bot ever!'
 DELAY_HOURS = 4
@@ -41,7 +39,7 @@ if __name__ == '__main__':
     token = os.environ['TELEGRAM_BOT_TOKEN']
     chat_id = os.environ['TELEGRAM_CHAT_ID']
     file_to_send = os.environ.get('FILE_TO_SEND', None)
-    delay = os.environ.get('DELAY_HOURS', DELAY_HOURS)
+    delay = os.environ.get('DELAY_HOURS', DELAY_HOURS)*3600
     
     directories = [Path.cwd().joinpath("images"),
                    Path.cwd().joinpath("EPIC"),
@@ -60,10 +58,11 @@ if __name__ == '__main__':
             img_data = get_image(random_image)         
     else:
         img_data = get_image(random_image)
+    
     send_message(TEXT, chat_id)
       
     while True:
-        time.sleep(float(delay)*3600)
+        time.sleep(float(delay))
         dir_num = random.randint(0, 2)
         images = os.listdir(directories[dir_num])
         random_image = directories[dir_num].joinpath(random.choice(images))
