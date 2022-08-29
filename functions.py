41d74d2f6d6ac48bd48e64aedc30697b728c261e
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-30 00:47:20 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,26 +1,23 @@
 import os
 import requests
-import urllib
 
-from datetime import date, datetime, timedelta
-from pathlib import Path
-from urllib.parse import urlparse, unquote, urlencode
+from urllib.parse import urlparse, unquote
 
 
 def parse_date(date):
-    day = date.strftime('%d')
-    month = date.strftime('%m')
-    year = date.strftime('%Y')
+    day = date.strftime("%d")
+    month = date.strftime("%m")
+    year = date.strftime("%Y")
     return year, month, day
 
 
 def download_img(img_url, download_path, params=None):
     response = requests.get(img_url, params=params)
     response.raise_for_status()
-    with open(download_path, 'wb') as file:
+    with open(download_path, "wb") as file:
         file.write(response.content)
 
-    
+
 def get_file_extension(url):
     parsed_url = urlparse(url)
     url_path = unquote(parsed_url.path)
@@ -30,9 +27,9 @@ def get_file_extension(url):
 
 def get_image(path):
     try:
-        with open(path, 'rb') as image:
+        with open(path, "rb") as image:
             img_data = image.read()
     except FileNotFoundError:
-        print('Файл не найден!')
+        print("Файл не найден!")
         raise
     return img_data
