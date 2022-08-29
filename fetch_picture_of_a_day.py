ed181ef0bf131b25fa7a265da8037715eaa5754a
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-30 00:45:32 +0300

    Update fetch_picture_of_a_day.py

--- fetch_picture_of_a_day.py
+++ fetch_picture_of_a_day.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from urllib.parse import urlencode
 
 
-URL = 'https://api.nasa.gov/planetary/apod'
+URL = "https://api.nasa.gov/planetary/apod"
 
 FOLDER_TO_DOWNLOAD = "APOD"
 
@@ -22,26 +22,26 @@ def get_APOD(url, query, download_dir_name):
     response = requests.get(url, params=params)
     response.raise_for_status()
     for item, elem in enumerate(response.json()):
-        if elem['media_type'] == 'image':
-            img_url = elem['url']
-            file_name = f'nasa_apod_{str(item)}{get_file_extension(img_url)}'
+        if elem["media_type"] == "image":
+            img_url = elem["url"]
+            file_name = f"nasa_apod_{str(item)}{get_file_extension(img_url)}"
             download_path = img_download_dir.joinpath(file_name)
             download_img(img_url, download_path)
 
-            
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     load_dotenv()
-    nasa_api_key = os.environ['NASA_API_KEY']
-    parser = argparse. ArgumentParser()
-    parser.add_argument('image_count',
-                        help="number of images to download",
-                        nargs='?',
-                        default=5,
-                        type=int)
+    nasa_api_key = os.environ["NASA_API_KEY"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "image_count",
+        help="number of images to download",
+        nargs="?",
+        default=5,
+        type=int,
+    )
 
     count = min(parser.parse_args().image_count, 100)
-     
-    params = {'api_key': nasa_api_key,
-              'count': count
-              }
+
+    params = {"api_key": nasa_api_key, "count": count}
     get_APOD(URL, params, FOLDER_TO_DOWNLOAD)
